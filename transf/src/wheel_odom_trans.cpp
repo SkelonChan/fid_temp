43225391340530c875c6a7e33bcb8ac715913797
Skelon_Chan <37016782+SkelonChan@users.noreply.github.com>
2022-06-20 08:21:56 +0800

    对编码器生成里程计数据重新编写

--- transf/src/wheel_odom_trans.cpp
+++ transf/src/wheel_odom_trans.cpp
@@ -3,7 +3,9 @@
 #include <transf/encoder.h>
 #include <nav_msgs/Odometry.h>
 #include <tf2/LinearMath/Quaternion.h>
-
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h" 
+#include <Eigen/Dense>
 
 double yaw = 0;
 double xt = 0;
@@ -57,12 +59,52 @@ void enc2odom::enc_sub_cb(const transf::encoder::ConstPtr &p)
     odo.pose.pose.orientation.z = qtn.getZ();
     odo.pose.pose.orientation.w = qtn.getW();
 
+    odo.pose.covariance[0] = 0.1;//x
+    odo.pose.covariance[7] = 0.1;//y
+    odo.pose.covariance[14] = 0.1; //z
+    odo.pose.covariance[21] = 0.1; //r
+    odo.pose.covariance[28] = 0.1; //p
+    odo.pose.covariance[35] = 0.1; //theta
+
+    odo.twist.covariance[0] = 0.2;
+    odo.twist.covariance[7] = 0.2;
+    odo.twist.covariance[14] = 0.2;
+    odo.twist.covariance[21] = 0.2;
+    odo.twist.covariance[28] = 0.2;
+    odo.twist.covariance[35] = 0.2;
+
+
+    odo.header.frame_id = "origin";
+    odo.header.stamp = ros::Time::now();
+    odo.child_frame_id = "odom_vehicle_base";
     odo_pub.publish(odo);
+
+    static tf2_ros::TransformBroadcaster bro;
+    geometry_msgs::TransformStamped tfs;
+
+    tfs.header.frame_id = "origin";
+    tfs.header.stamp = ros::Time::now();
+
+    tfs.child_frame_id = "odom_vehicle_base";
+    
+    tfs.transform.translation.x = xt;
+    tfs.transform.translation.y = yt;
+    tfs.transform.translation.z = 0.0; // 二维实现，pose 中没有z，z 是 0
+    //  |--------- 四元数设置
+
+    tfs.transform.rotation.x = qtn.getX();
+    tfs.transform.rotation.y = qtn.getY();
+    tfs.transform.rotation.z = qtn.getZ();
+    tfs.transform.rotation.w = qtn.getW();
+
+
+    //  5-3.广播器发布数据ca
+    bro.sendTransform(tfs);
 }
 
 void enc2odom::ser_sub_cb(const std_msgs::Int16::ConstPtr &p)
 {
-    theta = (-(p->data) + 90)*3.14159 /180;
+    theta = -(-(p->data) + 90)*3.14159 /180;
 }
 
 
